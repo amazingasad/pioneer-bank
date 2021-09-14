6a472ae5c5a835777648c09731c665634399f4ac
amazingasad <asadme631@gmail.com>
2021-09-14 00:38:38 -0700

    Added current deposit , withdraw & balance  status

--- script.js
+++ script.js
@@ -11,9 +11,31 @@ const depositBtn = document.getElementById("deposit-button");
 depositBtn.addEventListener("click", function(){
     const depositAmount = document.getElementById("deposit-amount").value;
     const depositNumber = parseFloat(depositAmount);
-    const currentDeposit = document.getElementById("current-deposit").innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-    document.getElementById("current-deposit").innerText = totalDeposit;
+    updateSpanText("current-deposit", depositNumber);
+    updateSpanText("current-balance", depositNumber);
     document.getElementById("deposit-amount").value = ""
 })
+
+// WITHDRAW BUTTON EVENT HANDLER
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawAmount = document.getElementById("withdraw-amount").value;
+    const withdrawAmountNumber = parseFloat(withdrawAmount);
+    const currentWithdraw = document.getElementById("current-withdraw").innerText;
+    const currentWithdrawNumber = parseFloat(currentWithdraw);
+    const currentBalance = document.getElementById("current-balance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance)
+    const totalWithdraw = currentBalanceNumber - withdrawAmountNumber;
+    document.getElementById("current-withdraw").innerText = withdrawAmountNumber + currentWithdrawNumber;
+    document.getElementById("current-balance").innerText = totalWithdraw;
+    document.getElementById("withdraw-amount").value = "";
+
+})
+
+
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
